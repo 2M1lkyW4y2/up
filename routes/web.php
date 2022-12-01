527e9722bfeda89729ab4c3ae66a9d788634477a
Милана <milana.moskovskikh@mail.ru>
2022-12-01 02:19:25 -0800

    uhigu

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', function () { return 'главная страница сайта';
 });
