c5350cbd51cfea1315e573cac34a71e5f182c8a2
Милана <milana.moskovskikh@mail.ru>
2022-12-09 04:24:30 -0800

    09.12

--- resources/views/test/sait.blade.php
+++ resources/views/test/sait.blade.php
@@ -5,29 +5,30 @@
         <div class="d-flex align-items-center justify-content-around"><h3>О нас</h3></div>
         <div class="d-flex align-items-center justify-content-center pt-5">
         <img src="\public\ТЗ\logo.svg" alt="">
+        <link rel="stylesheet" href="/public/css/style.css">
         </div>
     </div>
     <section>
         <div class="container">
-            <div class="d-flex">
+            <div class="d-flex justify-content-center aligh-items-center">
             <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 </div>
-                <div class="carousel-inner age">
+                <div class="carousel-inner ">
                     @foreach($comic as $comi)
                     @if($loop->first)
-                    <div class="carousel-item active">
-                    <img src="{{$comi->img}}" class="d-block w-100" alt="">
+                    <div class="carousel-item active age">
+                    <img class="img-fluid" src="{{$comi->img}}" class="d-block " alt="">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>{{$comi->name}}</h5>
                         <p>{{$comi->price}}</p>
                     </div>
                     </div>
                     @else
-                    <div class="carousel-item">
-                    <img src="{{$comi->img}}" class="d-block w-100" alt="...">
+                    <div class="carousel-item age">
+                    <img class="img-fluid" src="{{$comi->img}}" class="d-block " alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>{{$comi->name}}</h5>
                         <p>{{$comi->price}}</p>
